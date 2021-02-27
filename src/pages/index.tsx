5d554c8d31d907272cd4a61e1aa5bbd92171b149
Priscila Ravanelli <priscila.andreani@gmail.com>
2021-02-27 18:58:05 -0300

    add Countdown and styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,24 @@
+import Head from 'next/head';
 import { CompletedChallanges } from '../components/CompletedChallanges/CompletedChallanges/CompletedChallanges';
+import { Countdown } from '../components/Countdown/Countdown';
 import { ExperienceBar } from '../components/ExperienceBar/ExperienceBar';
 import { Profile } from '../components/Profile/Profile';
 import styles from '../styles/pages/Home.module.css';
 
+
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Moveit!</title>
+      </Head>
       <ExperienceBar />
 
       <section>
         <div>
           <Profile />
           <CompletedChallanges />
+          <Countdown />
         </div>
       </section>
     </div>
